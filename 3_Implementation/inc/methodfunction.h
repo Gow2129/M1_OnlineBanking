81409cf146f595a85feb313c44190b4280af44bc
Gow2129 <101723597+Gow2129@users.noreply.github.com>
2022-03-29 22:43:17 +0530

    Add files via upload

---
+++ 3_Implementation/inc/methodfunction.h
@@ -0,0 +1,22 @@
+#ifndef METHODFUNCTIONS_H_INCLUDED
+#define METHODFUNCTIONS_H_INCLUDED
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//int functions(int Methodchoice);
+//int existingrecord();
+void reg();
+void login();
+
+
+void fun1();
+void fun2();
+void fun3();
+void fun4();
+void fun5();
+
+
+
+
+#endif // METHODFUNCTIONS_H_INCLUDED
---
+++ 3_Implementation/inc/user.h
@@ -0,0 +1,16 @@
+#ifndef USER_H_INCLUDED
+#define USER_H_INCLUDED
+
+
+ struct userDetails{
+    char name[50];
+	char phone[50];
+	char ac[50];
+	char password[50];
+	float balance;
+
+};
+
+    //struct userDetails user,usr;
+
+#endif // USER_H_INCLUDED
