881ff585b0c4cc352295a272494f0867a359f423
Gow2129 <101723597+Gow2129@users.noreply.github.com>
2022-03-30 17:24:56 +0530

    Add files via upload

---
+++ 3_Implementation/test/test_OB.c
@@ -0,0 +1,36 @@
+#include "unity.h"
+
+#include "methodfunction.h"
+#include "user.h"
+
+
+
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+
+
+
+/* Prototypes for all the test functions */
+
+
+void test_Topicindex(void){
+        
+  	TEST_ASSERT_EQUAL(0,reg());
+        TEST_ASSERT_EQUAL(0,login());
+}
+
+
+
+
+int main(void)
+{
+UNITY_BEGIN();
+RUN_TEST(test_Topicindex);
+
+return UNITY_END();
+}
